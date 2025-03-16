c9fcd6d5212983108b2133c074febd5ee004db87
Waled Karim <waledkarim445@gmail.com>
2025-03-16 15:39:44 +0600

    hero section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Nav} from './Components';
+import Hero from './sections/Hero';
+import PopularProducts from './sections/PopularProducts';
+import SuperQuality from './sections/SuperQuality';
+import Services from './sections/Services';
+import SpecialOffer from './sections/SpecialOffer';
+import CustomerReviews from './sections/CustomerReviews';
+import Subscribe from './sections/Subscribe';
+import Footer from './sections/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='relative'>
+
+        <Nav />
+
+        <section className='xl:padding-l wide:padding-r padding-b'>
+          <Hero />
+        </section>
+
+        <section className='padding'>
+          <PopularProducts />
+        </section>
+
+        <section className='padding'>
+          <SuperQuality />
+        </section>
+
+        <section className='padding-x py-10'>
+          <Services />
+        </section>
+
+        <section className='padding'>
+          <SpecialOffer />
+        </section>
+
+        <section className='bg-pale-blue padding'>
+          <CustomerReviews />
+        </section>
+
+        <section className='padding-x sm:py-32 py-16 w-full'>
+          <Subscribe />
+        </section>
+
+        <section className=' bg-black padding-x padding-t pb-8'>
+          <Footer />
+        </section>
+
+    </main>
   )
 }
-
-export default App
